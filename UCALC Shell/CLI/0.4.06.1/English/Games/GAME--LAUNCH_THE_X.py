6a637bf34595038965e69217a437939ee8b2f8cb
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-30 14:48:56 -0700

    8 game files
    8 Game files for this version of UCALC.

---
+++ UCALC Shell/CLI/0.4.06.1/English/Games/GAME--LAUNCH_THE_X.py
@@ -0,0 +1,18 @@
+gameselectID = int(11)
+if gameselectID == 11:
+	if gameselectID == 11:
+		if gameselectID == 11:
+			if gameselectID == 11:
+				if gameselectID == 11:
+					if gameselectID == 11:
+						print ("Launch the X")
+						print ("A game for the delusional")
+						print ("\n\n")
+						launchx1 = input("Press [ENTER] key to start")
+						megalaunch = (1)
+						while megalaunch == (1):
+							print ("   x   ")
+							launchx2 = input("Press [ENTER] to launch the X")
+							print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+							print ("All Xcrafts have been launched")
+							exit = input("Press [ENTER] key to quit")
