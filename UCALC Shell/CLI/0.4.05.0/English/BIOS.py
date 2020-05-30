0fb5f2255bde160bc81c1f64fa14d7114dbc5f3a
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-29 18:25:29 -0700

    The last 38 files
    This is the last batch containing 38 files for this version of English UCALC.

---
+++ UCALC Shell/CLI/0.4.05.0/English/BIOS.py
@@ -0,0 +1,52 @@
+	if (biospar == "B3"):
+		bioloop = int(0)
+		print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+		while (bioloop == 0):
+			print ("====================================================================================>")
+			print ("UCALC System Basic Input Output System")
+			print ("Model ID: " + str(modID) + "\t\tLoop ID: " + str(konlop))
+			print ("Administrator username: " + str(adminusr) + "\nType 'ADMU' to modify")
+			print ("Administrator password: " + str(adminpas) + "\nType 'ADMP' to modify")	
+			modibio = str(input("Enter a modification code\nType Q to exit: "))
+			if (modibio == "ADMU"):
+				print ("Administrator account")
+				adminusr = str(input("Rename the administrator: "))
+			if (modibio == "ADMP"):
+				print ("Administrator password")
+				adminpas = str(input("Change the password: "))
+			if (modibio == "Q"):
+				(bioloop == 1)
+				if (bioloop == 1):
+					if (bioloop == 1):
+						print ("Cannot exit BIOS, Looping Error\nSorry :|")
+			print ("Error, unknown command")
+			'''
+	print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print ("Presentation build for:\nMrs. Nelson's pre-algebra class\nAt Walla Walla High School")
+	bootpas = input("Press [ENTER] key to start...")
+	print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	'''
+	# boot menu
+	print ("Ultimate calculator")
+	konloop = int(konlop)
+	#while konloop == 1: # please do not re-enable konloop unless you are going to fix it
+	print ("Copyleft Sean Walla Walla 2015-2018")
+	print ("\nReading from UCALC.UCALC_ROM")
+	more1 = input("Ready to start!\nPress [ENTER] key to enter the main menu")
+	''' pre 3.61 main menu script (1 line removed)
+	print ("| My calculators  [ID: 1] |")
+	print ("| Setup (old)     [ID: 2] |")
+	print ("| Wiki            [ID: 3] |")
+	print ("| Exit            [ID: 4] |")
+	print ("| Public calc     [ID: 5] |")
+	print ("| Games           [ID: 6] |")
+	print ("| Explorer        [ID: 7] |")
+	print ("| Accessories     [ID: 8] |")
+	print ("| Settings        [ID: 9] |")
+	print ("|                         |")
+	print ("\\                         /")
+	print (" \\=======================/")
+	print ("  \\ Version 0.01 alpha  /")
+	print ("   \\ developer copy    /")
+	print ("    \\=================/")
+	'''
---
+++ UCALC Shell/CLI/0.4.05.0/English/Currency_CALC.py
@@ -0,0 +1,89 @@
+				if calcIDSes == 10:
+					print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+					print ("Currency calculator")
+					print ("Select a currency to get started")
+					print ("$ - ID: 1")
+					print ("¢ - ID: 2")
+					print ("¥ - ID: 3")
+					print ("£ - ID: 4")
+					currencyID = int(input("Enter an ID (1-4): "))
+					if (currencyID == 1 or 2 or 3 or 4):
+						print ("Currency verified")
+						currencystr = str
+						if (currencyID == 1):
+							currencystr == ("$")
+							print ("Currency selected: $")
+						if (currencyID == 2):
+							currencystr == ("¢")
+							print ("Currency selected: ¢")
+						if (currencyID == 3):
+							currencystr == ("¥")
+							print ("Currency selected: ¥")
+						if (currencyID == 4):
+							currencystr == ("£")
+							print ("Currency selected: £")
+						print ("\n\n")
+						currencycalc1 = float(input("Enter an amount: "))
+						currencycalc2 = int(input("Select a calculation type:\n| + | ID = 1 | - | ID = 2 | * | ID = 3 | / | ID = 4 | % | ID = 5 | "))
+						currencycalc3 = float(input("Enter second amount: "))
+						if (currencycalc2 == 1 and currencyID == 1):
+							currencytotal = float(currencycalc1 + currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "$")
+						if (currencycalc2 == 1 and currencyID == 2):
+							currencytotal = float(currencycalc1 + currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¢")
+						if (currencycalc2 == 1 and currencyID == 3):
+							currencytotal = float(currencycalc1 + currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¥")
+						if (currencycalc2 == 1 and currencyID == 4):
+							currencytotal = float(currencycalc1 + currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "£")
+						if (currencycalc2 == 2 and currencyID == 1):
+							currencytotal = float(currencycalc1 - currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "$")
+						if (currencycalc2 == 2 and currencyID == 2):
+							currencytotal = float(currencycalc1 - currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¢")
+						if (currencycalc2 == 2 and currencyID == 3):
+							currencytotal = float(currencycalc1 - currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¥")
+						if (currencycalc2 == 2 and currencyID == 4):
+							currencytotal = float(currencycalc1 - currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "£")
+						if (currencycalc2 == 3 and currencyID == 1):
+							currencytotal = float(currencycalc1 * currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "$")
+						if (currencycalc2 == 3 and currencyID == 2):
+							currencytotal = float(currencycalc1 * currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¢")
+						if (currencycalc2 == 3 and currencyID == 3):
+							currencytotal = float(currencycalc1 * currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¥")
+						if (currencycalc2 == 3 and currencyID == 4):
+							currencytotal = float(currencycalc1 * currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "£")
+						if (currencycalc2 == 4 and currencyID == 1):
+							currencytotal = float(currencycalc1 / currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "$")
+						if (currencycalc2 == 4 and currencyID == 2):
+							currencytotal = float(currencycalc1 / currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¢")
+						if (currencycalc2 == 4 and currencyID == 3):
+							currencytotal = float(currencycalc1 / currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¥")
+						if (currencycalc2 == 4 and currencyID == 4):
+							currencytotal = float(currencycalc1 / currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "£")
+						if (currencycalc2 == 5 and currencyID == 1):
+							currencytotal = float(currencycalc1 % currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "$")
+						if (currencycalc2 == 5 and currencyID == 2):
+							currencytotal = float(currencycalc1 % currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¢")
+						if (currencycalc2 == 5 and currencyID == 3):
+							currencytotal = float(currencycalc1 % currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "¥")
+						if (currencycalc2 == 5 and currencyID == 4):
+							currencytotal = float(currencycalc1 % currencycalc3)
+							print ("The current amount is: " + str(currencytotal) + "£")
+						bloat1 = input("| FAIL! SETUP RESTARTING (we can't fix the bug right now, we are porting features in. Stabilizing later, my bad)")
