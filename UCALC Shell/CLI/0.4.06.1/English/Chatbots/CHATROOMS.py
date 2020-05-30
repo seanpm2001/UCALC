9ba63df544777c795855f234d3f6a5550e49d6f3
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-30 14:33:15 -0700

    8 Chat bot data files
    8 Data files for the UCALC Chatbot system.

---
+++ UCALC Shell/CLI/0.4.06.1/English/Chatbots/CHATROOMS.py
@@ -0,0 +1,17 @@
+		if selectACessory == ("2"):
+			print ("Chatrooms")
+			print ("\n")
+			print ("|=====================|")
+			print ("| Join a room [ID: 1] |")
+			print ("|=====================|")
+			print ("\n")
+			print ("|===========================|")
+			print ("| Create a new room [ID: 2] |")
+			print ("|===========================|")
+			print ("\n\n\n\n\n\n")
+			roomJoiner = str(input("Enter the ID to join or create a room: "))
+			if roomJoiner == ("1"):
+				print ("No rooms found!")
+			if roomJoiner == ("2"):
+				print ("New room")
+	exit1 = input("Press [ENTER] key to exit")
