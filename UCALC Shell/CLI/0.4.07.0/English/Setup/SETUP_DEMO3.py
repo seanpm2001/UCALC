1cdb0bc2ef0f1fe4d15baad70e0ae2a31201e20a
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-30 16:06:59 -0700

    6 setup files
    6 setup files.

---
+++ UCALC Shell/CLI/0.4.07.0/English/Setup/SETUP_DEMO3.py
@@ -0,0 +1,127 @@
+# start of script
+print ("Setup demo")
+more1 = input("Press [ENTER] key to continue")
+print ("Language ")
+print ("English (1)")
+print ("Spanish (2) (unavailable)")
+print ("French (3) (unavailable")
+languageID = str(input(">> "))
+if (languageID == "1"):
+	print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print ("Welcome to the Ultimate calculator all-in-one edition setup wizard! ")
+	print ("\nThis wizard will help you through the installation of UCALC")
+	print ("\n\n\n\n\n")
+	start1 = input("Press [ENTER] key to begin")
+	print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	print (" ")
+	print ("System requirement checking ")
+	print ("\n\n\n1080 Megabytes (disk)")
+	print ("100 Megabytes (processor)")
+	print ("64 Megabytes (Random Access Memory)")
+	print ("\n\n\nYour system is compatible with UCALC All-in-one edition!")
+	start2 = input("Press [ENTER] key to continue")
+	loopStep = int(1)
+	while (loopStep == 1):
+		print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+		print ("Feature select")
+		print ("Select features you want: ")
+		print ("\n[ABOUTPAGES] (1)")
+		print ("[Accessories] (2)")
+		print ("[Account] (3)")
+		print ("[Chatbots] (4)")
+		print ("[Old clients] (5)")
+		print ("[Converters] (6)")
+		print ("[Documentation] (7)")
+		print ("[External links] (8)")
+		print ("[Games] (9)")
+		print ("[Historical calculators] (10)")
+		print ("[Modes] (11)")
+		print ("[Political] (12)")
+		print ("[Primitive Calculators] (13)")
+		print ("[PTOE Project] (14)")
+		print ("[Research] (15)")
+		print ("[Setup] (16)")
+		print ("[Teacher Features] (17)")
+		print ("[Quit] (18)")
+		featureID = str(input("Enter an ID to continue, enter as many features as you want, then enter 18 to continue"))
+		if (featureID == "1"):
+			print ("\n\n")
+			print ("Imported the [ABOUTPAGES] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "2"):
+			print ("\n\n")
+			print ("Imported the [Accessories] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")	
+		if (featureID == "3"):
+			print ("\n\n")
+			print ("Imported the [Account] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")	
+		if (featureID == "4"):
+			print ("\n\n")
+			print ("Imported the [Chatbots module to your installation")
+			continue1 = input("Press [ENTER] key to continue")	
+		if (featureID == "5"):
+			print ("\n\n")
+			print ("Imported the [Old Clients] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")		
+		if (featureID == "6"):
+			print ("\n\n")
+			print ("Imported the [Converters] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")			
+		if (featureID == "7"):
+			print ("\n\n")
+			print ("Imported the [Documentation] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "8"):
+			print ("\n\n")
+			print ("Imported the [External links] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "9"):
+			print ("\n\n")
+			print ("Imported the [Games] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "10"):
+			print ("\n\n")
+			print ("Imported the [Historical calculators] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "11"):
+			print ("\n\n")
+			print ("Imported the [Modes] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "12"):
+			print ("\n\n")
+			print ("Imported the [Political] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "13"):
+			print ("\n\n")
+			print ("Imported the [Primitive calculators] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "14"):
+			print ("\n\n")
+			print ("Imported the [PTOE Project] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "15"):
+			print ("\n\n")
+			print ("Imported the [Research] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "16"):
+			print ("\n\n")
+			print ("Imported the [Setup] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "17"):
+			print ("\n\n")
+			print ("Imported the [Teacher Features] module to your installation")
+			continue1 = input("Press [ENTER] key to continue")
+		if (featureID == "18"):
+			print ("\n\n")
+			(loopStep == loopStep - 1)
+			print ("All modules imported ")
+			continue1 = input("Press [ENTER] key to continue")	
+noMore = input("Press [ENTER] key to quit")
+'''''''''''''''''''''''''''''''''''''''
+| Trying to break this while loop     |
+'''''''''''''''''''''''''''''''''''''''
+# Sean Myrick on 1/3/2019 at 11:13 am
+'''''''''''''''''''''
+| End of script     |
+'''''''''''''''''''''
