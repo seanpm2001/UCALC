62a6e32b91fccdbec4254431ccb668fa14415c1a
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-30 14:35:54 -0700

    16 conversion tools
    16 conversion tools for this version of UCALC.

---
+++ UCALC Shell/CLI/0.4.06.1/English/Converters/RANDOM_NUM_GENERATOR.py
@@ -0,0 +1,12 @@
+		if calcIDSes == 50:
+					print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+					print ("Random Number Generator")
+					ranrange = int(input("How many random numbers do you want to generate (1-100 MAX recommended): "))
+					rand1 = int(input("Enter the start number of the random range: "))
+					rand2 = int(input("Enter the end number of the random range: "))
+					staran = int
+					staran = 0
+					while not (staran == ranrange):
+						ranrange - 1
+						print (random.randint(rand1, rand2))
+					bloat1 = input("| FAIL! SETUP RESTARTING (we can't fix the bug right now, we are porting features in. Stabilizing later, my bad) ")	
