259ef702e696aaa71a015de124e070622678235e
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-30 15:03:29 -0700

    2 basic scripts for teachers
    2 basic scripts for teachers.

---
+++ UCALC Shell/CLI/0.4.06.1/English/Teacher features/GRAPH_CALC.py
@@ -0,0 +1,7 @@
+		if calcIDSes == 20:
+					print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+					print ("Graph")
+					print ("This mode is currently unavailable. Sorry for the inconvenience ")
+					print ("We are still working on adding in new features and stabilizing the program. Please install a newer version to use graphing")
+					print ("No error, just not here yet")
+					bloat1 = input("| FAIL! SETUP RESTARTING (we can't fix the bug right now, we are porting features in. Stabilizing later, my bad)")
