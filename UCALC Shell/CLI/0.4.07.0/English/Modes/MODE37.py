8d18b8f95f5ae48f7f052fb76efc6117d4e58258
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-30 15:53:50 -0700

    15 mode files
    15 mode files

---
+++ UCALC Shell/CLI/0.4.07.0/English/Modes/MODE37.py
@@ -0,0 +1,4 @@
+		if calcIDSes == 37:
+					print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+					print ("")
+					bloat1 = input("| FAIL! SETUP RESTARTING (we can't fix the bug right now, we are porting features in. Stabilizing later, my bad)")
